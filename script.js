c12c8187267cfc3ed803818ece589513abbb6e48
gabrielps1 <gabrielpereirasilva9@hotmail.com>
2024-09-27 11:28:07 -0300

    Nesse commit estou enviando a lógica pronta

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Olá")
+let nome = "Gabriel"
+let xp = 9400
+let nivel = ""
+
+if (xp < 1000)
+    console.log(nivel = "Ferro")
+else if (xp >= 1001 && xp <= 2000)
+    console.log(nivel = "Bronze")
+else if (xp >= 2001 && xp <= 5000)
+    console.log(nivel = "Prata")
+else if (xp >= 6001 && xp <= 7000)
+    console.log(nivel = "Ouro")
+else if (xp >= 7001 && xp <= 8000)
+    console.log(nivel = "Platina Diamante")
+else if (xp >= 8001 && xp <= 9000)
+    console.log(nivel = "Ascendente")
+else if (xp >= 9001 && xp <= 10000)
+    console.log(nivel = "Importal")
+else if (xp >= 10001)
+    console.log(nivel = "Radiante")
+
+console.log("O Herói de nome: " + nome + " está no nivel: " + nivel)
